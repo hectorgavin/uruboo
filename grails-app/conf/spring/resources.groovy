eae927aa55e1c220cf0588090b28aafe0f76196f
Héctor Gavín Rivas <hector.gavin.rivas@gmail.com>
2015-12-13 23:31:39 +0100

    Added generic code for exluding class in all domain json requests

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,21 +1,15 @@
 // Place your Spring DSL code here
 
-import com.uruboo.*
 import grails.rest.render.json.JsonRenderer
 import grails.rest.render.json.JsonCollectionRenderer
 
 beans = {
-    assistantJSONRenderer(JsonRenderer, Assistant) {
-        excludes = ['class']
-    }
-    assistantJSONCollectionRenderer(JsonCollectionRenderer, Assistant) {
-        excludes = ['class']
-    }
-
-    customerJSONRenderer(JsonRenderer, Customer) {
-        excludes = ['class']
-    }
-    customerJSONCollectionRenderer(JsonCollectionRenderer, Customer) {
-        excludes = ['class']
+    for (domainClass in grailsApplication.domainClasses) {
+        "${domainClass.shortName}JSONRenderer"(JsonRenderer, domainClass.clazz) {
+            excludes = ['class']
+        }
+        "${domainClass.shortName}JSONCollectionRenderer"(JsonCollectionRenderer, domainClass.clazz) {
+            excludes = ['class']
+        }
     }
 }
